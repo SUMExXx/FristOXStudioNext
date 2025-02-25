eabb6c8df32e0ccd0bf32d62e04b3abe946052ee
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2025-02-26 02:41:52 +0530

    minor fix

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,5 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  return NextResponse.json({ userId: params.id });
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  return NextResponse.json({ message: `User ID: ${params.id}` });
 }
+
