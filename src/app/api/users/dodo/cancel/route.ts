8c2e0f62a0558b28140f08464c3657626d0667ce
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2025-06-20 15:02:36 +0530

    minor fix

--- src/app/api/users/dodo/cancel/route.ts
+++ src/app/api/users/dodo/cancel/route.ts
@@ -1,41 +1,41 @@
-import { updateSubscriptionInDatabase } from "@/lib/api-functions";
-import dodo from "@/lib/dodo";
+// // import { updateSubscriptionInDatabase } from "@/lib/api-functions";
+// import dodo from "@/lib/dodo";
 
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    const email = session?.user?.email;
-    const subscriptionId = body.subscriptionId;
+// export async function POST(req: NextRequest) {
+//   try {
+//     const body = await req.json();
+//     const email = session?.user?.email;
+//     const subscriptionId = body.subscriptionId;
 
-    const response = await dodo.subscriptions.update(subscriptionId, {
-      status: "cancelled",
-      metadata: {} // metadata can be sent here 
-    })
-    console.log(response)
-    // Step 2: Update database
-    const result = await updateSubscriptionInDatabase(email!, subscriptionId);
+//     const response = await dodo.subscriptions.update(subscriptionId, {
+//       status: "cancelled",
+//       metadata: {} // metadata can be sent here 
+//     })
+//     console.log(response)
+//     // Step 2: Update database
+//     const result = await updateSubscriptionInDatabase(email!, subscriptionId);
 
-    if (!result.success) {
-      return NextResponse.json(
-        { error: result.error?.message },
-        { status: result.error?.status }
-      );
-    }
+//     if (!result.success) {
+//       return NextResponse.json(
+//         { error: result.error?.message },
+//         { status: result.error?.status }
+//       );
+//     }
 
-    return NextResponse.json({
-      success: true,
-      message: "Subscription cancelled and database updated successfully",
-    });
-  } catch (error) {
-    console.error("Error during subscription cancellation:", error);
-    return NextResponse.json(
-      {
-        error: "Internal server error",
-        details: error instanceof Error ? error.message : "Unknown error occurred",
-      },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({
+//       success: true,
+//       message: "Subscription cancelled and database updated successfully",
+//     });
+//   } catch (error) {
+//     console.error("Error during subscription cancellation:", error);
+//     return NextResponse.json(
+//       {
+//         error: "Internal server error",
+//         details: error instanceof Error ? error.message : "Unknown error occurred",
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
